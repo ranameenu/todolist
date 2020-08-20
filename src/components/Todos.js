2d9e1a3de5228f98a1ba56291ab377640689de45
user <user@WKS27042.hm.com>
2020-08-21 00:11:01 +0530

    ui

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,6 +4,8 @@ class Todos extends Component {
   render() {
     return (
       <div>
+        <h1 className="text-center mb-4">Todo List</h1>
+
         <div className="card">
           <div className="card-body">
             <div className="form-group">
@@ -14,6 +16,32 @@ class Todos extends Component {
               />
             </div>
             <button className="btn btn-primary btn-sm">Add Todo</button>
+          </div>
+        </div>
+
+        <div className="card mt-4">
+          <div className="card-body">
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search"
+              />
+            </div>
+
+            <hr />
+
+            <ul className="list-group mt-5">
+              <li className="list-group-item">
+                Todo 1
+                <button className="btn btn-danger btn-sm ml-2 float-right">
+                  delete
+                </button>
+                <button className="btn btn-secondary btn-sm float-right">
+                  complete
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
